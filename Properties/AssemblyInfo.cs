8e203d5943add474946c0d96b96f6ac52909bc07
elmagnifico <elmagnificogg@gmail.com>
2022-04-17 04:17:22 +0800

    add auto version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,34 +1,21 @@
-﻿using System.Reflection;
+﻿
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
+// General Information
 [assembly: AssemblyTitle("DCTracker")]
 [assembly: AssemblyDescription("Diablo Clone Tracker")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("elmagnifico")]
 [assembly: AssemblyProduct("DCTracker")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("opyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// 将 ComVisible 设置为 false 会使此程序集中的类型
-//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
-//请将此类型的 ComVisible 特性设置为 true。
-[assembly: ComVisible(false)]
+// Version informationr(
+[assembly: AssemblyVersion("1.0.0.11")]
+[assembly: AssemblyFileVersion("1.0.0.11")]
+[assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("2b1e1588-2231-47f0-b222-3949228dd30a")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
-//[assembly: AssemblyVersion("2.10.*")]
